b9ff14ffb9d44ca7277826a6488b26a38c0850b4
Neha <nehamyadav22@gmail.com>
2023-12-25 22:58:14 +0530

    Completed User RestFul API & tested

--- techTalent-Network/src/main/java/project/techTalent/Network/controller/UserController.java
+++ techTalent-Network/src/main/java/project/techTalent/Network/controller/UserController.java
@@ -1,9 +1,14 @@
 package project.techTalent.Network.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import project.techTalent.Network.payloads.ApiResponse;
 import project.techTalent.Network.payloads.UserDto;
 import project.techTalent.Network.services.UserService;
 
@@ -31,14 +37,46 @@ public class UserController
 	
 	//Put - update user
 	
-//	@PutMapping("/update")
-//	public ResponseEntity<UserDto> updateUser(@RequestBody  UserDto userDto, Integer Id)
-//	{
-//		UserDto updatedUser = this.userService.updateUser(userDto, Id);
-//		return new ResponseEntity<>(updatedUser , HttpStatus.CREATED);
-//		
-//	}
+@PutMapping("/{userId}") 
+	public ResponseEntity<UserDto> updateUser(@RequestBody  UserDto userDto, @PathVariable("userId") Integer uId)
+	{
+		UserDto updatedUser = this.userService.updateUser(userDto, uId);
+		return  ResponseEntity.ok(updatedUser);
+    }
 	
+//Delete- delete user
+
+@DeleteMapping("/{userId}")
+public ResponseEntity<ApiResponse> deleteUser(@PathVariable("userId") Integer uid)
+{
+	this.userService.deleteUser(uid);
+	return new ResponseEntity<ApiResponse>(new ApiResponse("User deleted successfully",true), HttpStatus.OK);
+}
+
+@GetMapping("/")
+public ResponseEntity <List<UserDto>> getAllUser()
+{
+	List<UserDto> getAlluser = this.userService.getAllUser();
+	return  ResponseEntity.ok(getAlluser);
+//	
+//	return ResponseEntity.ok(this.userService.getAllUser());
+}
+
+//Get - Single user get 
+
+@GetMapping("/{userId}")
+public ResponseEntity<UserDto> getSingleUser(@PathVariable Integer userId)
+{
+//	UserDto getUser = this.userService.getUserById(userId);
+//	return new ResponseEntity<UserDto>(getUser, HttpStatus.ACCEPTED);	
+	
+	return ResponseEntity.ok(this.userService.getUserById(userId));
+}
+
+
+
+
+
 	
 	
 	
--- techTalent-Network/src/main/java/project/techTalent/Network/respositories/Implementation/UserServiceImpl.java
+++ techTalent-Network/src/main/java/project/techTalent/Network/respositories/Implementation/UserServiceImpl.java
@@ -1,7 +1,6 @@
 package project.techTalent.Network.respositories.Implementation;
 
 import java.util.List;
-
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
